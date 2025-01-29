636f9ff7436748b12f82639b4f6c346f87c76406
Christian Ruettimann <chris@k3s01.sun.bitbull.ch>
2025-01-29 11:47:44 +0100

    testing

---
+++ setup.sh
@@ -0,0 +1,38 @@
+
+
+export NAMESPACE=web-ui
+export FQDN=ki.domain.tld
+
+test -d runtime && echo ERROR remove ./runtime dir first
+test -d ./runtime && exit 1
+mkdir ./runtime
+chmod 700 ./runtime
+for y in *.yml
+do
+  cp -av $y ./runtime/$y
+  sed -i "s/__NAMESPACE__/$NAMESPACE/g"      ./runtime/$y
+  sed -i "s/__FQDN__/$FQDN/g"                ./runtime/$y
+done
+
+echo "# current vars
+NAMESPACE=$NAMESPACE
+FQDN=$FQDN
+" > ./runtime/env.sh
+
+ls -l ./runtime
+
+echo enter to appy
+read x
+
+for y in runtime/*.yml
+do
+  echo $y
+  kubectl apply -f $y
+done
+
+echo "in container run:
+cd /app/backend/data
+ollama run deepseek-r1:1.5b
+
+
+"
